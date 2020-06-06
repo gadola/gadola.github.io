b604b039dce54986b49c26c924bce352ac1ff30c
hoangnam <efert269@gmail.com>
2020-06-06 15:42:58 +0700

    no read file

--- uploadinfo.php
+++ uploadinfo.php
@@ -6,10 +6,10 @@ fwrite($userFile, $_POST['ten'].'|');
 fwrite($userFile, $_POST['sdt'].'|');
 fwrite($userFile, $_POST['linkfb']."\n");
 
-    $read = file('php.txt');
-    foreach ($read as $line) {
-    echo $line;
-    }
+    // $read = file('php.txt');
+    // foreach ($read as $line) {
+    // echo $line;
+    // }
 fclose($userFile);
 // echo '<pre>';
 // print_r( $_POST);
